db8ceec4f2333528c28112f77419b8b2c4e25b11
weir99 <10331058+weir99@users.noreply.github.com>
2022-05-24 09:56:23 -0500

    Setup basic console view

---
+++ src/views/consoleview.cs
@@ -0,0 +1,25 @@
+namespace Views;
+using Models; using Updates; using System;
+
+class ConsoleView : View{
+    Document? doc;
+
+    public ConsoleView(Document doc){
+        this.doc = doc;
+    }
+
+    public override void update(Update toUpdate)
+    {
+        if(toUpdate is InsertUpdate) writeDoc();
+    }
+
+    // Writes the entire document, we'll just use this for now
+    private void writeDoc(){
+        Console.SetCursorPosition(0, 0);
+        if(doc is not null){
+            foreach(var Line in doc.Text){
+                Console.WriteLine(Line);
+            }
+        }
+    }
+}
