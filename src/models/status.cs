f2a94d28290429f1fa0aa699118b145642465845
weir99 <10331058+weir99@users.noreply.github.com>
2022-05-24 08:39:46 -0500

    Setup basic status bar

---
+++ src/models/status.cs
@@ -0,0 +1,32 @@
+namespace Models;
+
+// Contains info on current editing status, probably going to keep pretty basic for now
+class Status{
+    public const string INSERT_STATUS = "INSERT";
+    public const string NORMAL_STATUS = "NORMAL"; 
+
+    private string statDisplay = NORMAL_STATUS;
+    private State stat  = State.Normal;
+
+    public void setInsert(){
+        statDisplay = INSERT_STATUS; 
+        stat = State.Insert;
+    }
+    public void setNormal(){
+        statDisplay = NORMAL_STATUS;
+         stat = State.Normal; 
+    }
+
+    public void setCommand(string commandText){
+        statDisplay = commandText;
+        stat = State.Command;
+    }
+
+}
+
+enum State{
+    Insert,
+    Normal,
+    Command
+}
+
