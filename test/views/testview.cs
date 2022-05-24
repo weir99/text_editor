1924271f7c783437c73eadc6c82ae9e40974b298
weir99 <10331058+weir99@users.noreply.github.com>
2022-05-24 10:48:40 -0500

    Wrote constructor for test view

--- test/views/testview.cs
+++ test/views/testview.cs
@@ -3,6 +3,8 @@ using Views; using Updates; using Models;
 
 class TestView : View{
     Document doc;
+
+    public TestView(Document doc) => this.doc = doc;
     public override void update(Update toUpdate)
     {
         return; // Not to worried right now, just handling insert
