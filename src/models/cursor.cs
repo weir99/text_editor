df8950ca5daa06798a7439ca775a6bb59f857ebb
weir99 <10331058+weir99@users.noreply.github.com>
2022-06-12 15:20:07 -0500

    Wrote copy constructor for cursor

--- src/models/cursor.cs
+++ src/models/cursor.cs
@@ -3,5 +3,10 @@ public class Cursor{
 
     public int xPosition {get; set;}
     public int yPosition {get; set;}
+
+    public Cursor(Cursor otherCursor){
+        this.xPosition = otherCursor.xPosition;
+        this.yPosition = otherCursor.yPosition;
+    }
 }
 
