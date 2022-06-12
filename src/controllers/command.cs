48087dae1bfe4caac59167e5ed969a3d3eb5ef4e
weir99 <10331058+weir99@users.noreply.github.com>
2022-06-12 15:20:43 -0500

    Made commands more complex, more control

--- src/controllers/command.cs
+++ src/controllers/command.cs
@@ -1,8 +1,34 @@
 namespace Commands;
+using Models;
 
 // Abstract type for handline actions
 // Will probably just be keypresses for now, but may want more complex (e.g. mouse movement) later
-public abstract class Command{}
+public abstract class Command{
+}
 
 // If for whatever reason we don't have an action to pass along, pass this
 public class NullCommand : Command{}
+
+// 
+// Below are the commands that we'll actually execute
+//
+
+
+//Basic command, does a thing, can't be undone
+public abstract class DoCommand : Command{
+    // The document to execute the command in
+    protected Document doc;
+    public DoCommand(Document doc){
+        this.doc = doc;
+    }
+    public abstract void @do();
+}
+
+// More complex command, can be undone
+public abstract class UndoCommand : DoCommand{
+    public UndoCommand(Document doc): base(doc){}
+    public abstract void @undo();
+}
+
+
+
