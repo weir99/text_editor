7cf17e1e12b1a1cd3bb773d9608ba26d6c2c8554
weir99 <10331058+weir99@users.noreply.github.com>
2022-06-14 15:02:58 -0500

    Added better constructor options for cursor

--- src/models/cursor.cs
+++ src/models/cursor.cs
@@ -3,7 +3,10 @@ public class Cursor{
 
     public int xPosition {get; set;}
     public int yPosition {get; set;}
-
+    public Cursor(int xPosition, int yPosition){
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
     public Cursor(Cursor otherCursor){
         this.xPosition = otherCursor.xPosition;
         this.yPosition = otherCursor.yPosition;
